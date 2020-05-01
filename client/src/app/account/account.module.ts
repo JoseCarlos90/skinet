537c52b5d35fa497afd2adf7c671a12a94b63861
Jose Carlos Gonzalez Horcajo <jossu_sw000@hotmail.com>
2020-05-01 17:58:21 +0200

    End of section 17

---
+++ client/src/app/account/account.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
+  ],
+  exports: [RouterModule]
+})
+export class AccountModule { }
